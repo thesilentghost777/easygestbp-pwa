d1d67d2d8780f4e96f8c3dbccd54a771a084aef7
root <root@ghost.ghost>
2026-01-20 20:23:25 +0100

    I believe in Jesus

--- src/lib/backend.ts
+++ src/lib/backend.ts
@@ -1,3 +1,5 @@
 //backend.ts
-const API_URL = 'http://192.168.43.202:8000/api';
+//const API_URL = 'http://192.168.43.202:8000/api';
+
+const API_URL = 'https://easygestbp.techforgesolution237.site/api';
 export { API_URL };
